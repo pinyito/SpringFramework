20aa2a4cf38f634ccd8d77875d50174c69396170
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-10 03:03:11 -0500

    Created a spring project.
    Used spring initializr to create a spring project to start with.

---
+++ srping-framework/src/main/java/com/eriapinyiothieno/srpingframework/SrpingFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.eriapinyiothieno.srpingframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrpingFrameworkApplication.class, args);
+	}
+
+}
